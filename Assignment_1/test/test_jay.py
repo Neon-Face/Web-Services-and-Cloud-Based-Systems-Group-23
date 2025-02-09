3fb53ee89c70a40505a2f897f1871d41bc838158
jjyuhub <jyu290@student.vu.nl>
2025-02-09 15:04:51 +0100

    Update test_jay.py port 8000

--- Assignment_1/test/test_jay.py
+++ Assignment_1/test/test_jay.py
@@ -7,7 +7,7 @@ import time
 
 
 class TestApi(unittest.TestCase):
-    base_url = "http://127.0.0.1:5000"
+    base_url = "http://127.0.0.1:8000"
 
     def populate_variables_from_csv(self):
         print("\n📂 Loading test data from CSV file...")
